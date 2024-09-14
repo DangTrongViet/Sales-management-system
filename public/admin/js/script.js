f996bf415165e78bb4deccb33070d768a6befa17
Trương Tiến Anh <truongtienanh16@gmail.com>
2024-09-14 18:29:20 +0700

    uopdate

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -171,15 +171,17 @@ if(formTrashItem){
 //---------------------------------------------------------------------
 //Show alert (Thông báo cập nhập thành công hay chưa)
  const showAlert = document.querySelector("[show-alert]")
- console.log(showAlert)
+
  if(showAlert){
     const dataTime = showAlert.getAttribute("data-time")
     const closeAlert = showAlert.querySelector("[close-alert]")
     setTimeout(()=>{
         showAlert.classList.add("alert-hidden")
     }, parseInt(dataTime))
-
+    
     closeAlert.addEventListener("click", ()=>{
         showAlert.classList.add("alert-hidden")
     })
+
  }
+
