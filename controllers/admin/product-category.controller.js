5c288ee7037da826069b19a01450903b2425f25b
Trương Tiến Anh <truongtienanh16@gmail.com>
2024-09-22 00:23:31 +0700

    Update

---
+++ controllers/admin/product-category.controller.js
@@ -0,0 +1,8 @@
+const Product = require("../../models/product.model.js")
+
+//[GET] /admin/products-category
+module.exports.index = async (req, res)=>{
+    res.render("admin/pages/products-category/index.pug", {
+        pageTitle: "Danh mục sản phẩm"
+    })
+}
