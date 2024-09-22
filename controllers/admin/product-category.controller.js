4cf98bc588d04ddd12a63e6a7c40d3e68b7cf450
Trương Tiến Anh <truongtienanh16@gmail.com>
2024-09-22 22:22:38 +0700

    Update

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -1,9 +1,17 @@
 const ProductCategory = require("../../models/product-category.model")
+const systemConfig = require("../../config/system")
 
 //1. [GET] /admin/products-category
 module.exports.index = async (req, res)=>{
+    let find = {
+        deleted: false
+    };
+
+    const records = await ProductCategory.find(find)
+
     res.render("admin/pages/products-category/index.pug", {
-        pageTitle: "Danh mục sản phẩm"
+        pageTitle: "Danh mục sản phẩm",
+        records: records
     })
 }
 
@@ -17,9 +25,14 @@ module.exports.create = async (req, res)=>{
 
 //[POST] /admin/products-category/create
 module.exports.createPost = async (req, res)=>{
-    console.log(req.body)
+    if(req.body.position == ""){
+        const countProducts = await ProductCategory.countDocuments()
+        req.body.position = countProducts + 1
+    }else{
+        req.body.position = parseInt(req.body.position)
+    }
 
-    const product = new ProductCategory(req.body)
-    product.save()
-    res.redirect("back")
+    const record = new ProductCategory(req.body)
+    await record.save()
+    res.redirect(`${systemConfig.prefixAdmin}/products-category`)
 }
