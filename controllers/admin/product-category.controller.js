a5121b0e61f51d096fec1e8edfa548f04a57caa3
Trương Tiến Anh <truongtienanh16@gmail.com>
2024-09-22 21:23:16 +0700

    Update

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -1,8 +1,21 @@
 const Product = require("../../models/product.model.js")
 
-//[GET] /admin/products-category
+//1. [GET] /admin/products-category
 module.exports.index = async (req, res)=>{
     res.render("admin/pages/products-category/index.pug", {
         pageTitle: "Danh mục sản phẩm"
     })
+}
+
+//2. Tạo danh mục
+//[GET] /admin/products-category/create
+module.exports.create = async (req, res)=>{
+    res.render("admin/pages/products-category/create.pug", {
+        pageTitle: "Danh mục sản phẩm"
+    })
+}
+
+//[POST] /admin/products-category/create
+module.exports.createPost = async (req, res)=>{
+    res.send("oke")
 }
--- routes/admin/product-category.route.js
+++ routes/admin/product-category.route.js
@@ -1,8 +1,23 @@
 const express = require('express')
 const router = express.Router()
 
-const controller = require("../../controllers/admin/product-category.controller")
+const multer  = require('multer'); //Thư viện để upload ảnh
+const upload = multer() 
 
+const controller = require("../../controllers/admin/product-category.controller")
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware.js")
+const validate = require("../../validate/admin/product.validate.js")
+
+//Tranh chủ
 router.get('/', controller.index)
+
+//Tạo danh mục
+router.get('/create', controller.create)
+router.post('/create',
+    upload.single('thumbnail'),
+    uploadCloud.upload,
+    validate.createPost,
+    controller.createPost)
+    
 
 module.exports = router
