ca8d59c33e90907e8cf89675982dd217df675296
Trương Tiến Anh <truongtienanh16@gmail.com>
2024-09-27 23:22:05 +0700

    Update

---
+++ controllers/admin/roles.controller.js
@@ -0,0 +1,15 @@
+const Roles = require("../../models/role.model")
+// const ProductCategory = require("../../models/product-category.model")
+// const filterStatusHelper = require("../../helpers/filterStatus.js")
+// const searchHelper = require("../../helpers/search.js")
+// const paginationHelper = require("../../helpers/pagination.js")
+// const systemConfig = require("../../config/system.js")
+// const createTreeHelper = require("../../helpers/createTree")
+
+
+module.exports.index = async (req, res)=>{
+    res.send("oke")
+    // res.render("admin/pages/roles/index.pug", {
+    //     pageTitle: "Nhóm quyền",
+    // })
+}
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,6 +1,7 @@
 const dashboardRoutes = require("./dashboard.route.js")
 const productRoutes = require("./product.route.js")
 const productCategoryRoutes = require("./product-category.route.js")
+const roleRoutes = require("./role.route.js")
 const systemConfig = require("../../config/system.js")
 
 module.exports = (app)=>{ 
@@ -8,4 +9,5 @@ module.exports = (app)=>{
     app.use(PATH_ADMIN + '/dashboard', dashboardRoutes)
     app.use(PATH_ADMIN + '/products',productRoutes )
     app.use(PATH_ADMIN + '/products-category',productCategoryRoutes)
+    app.use(PATH_ADMIN + '/roles',roleRoutes)
 }
