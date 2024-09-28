a7ac0d61ad2b6f87a4eea2c726137b2f3bd6712a
Trương Tiến Anh <truongtienanh16@gmail.com>
2024-09-28 21:46:30 +0700

    Update

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -3,12 +3,11 @@ const Roles = require("../../models/role.model")
 // const filterStatusHelper = require("../../helpers/filterStatus.js")
 // const searchHelper = require("../../helpers/search.js")
 // const paginationHelper = require("../../helpers/pagination.js")
-// const systemConfig = require("../../config/system.js")
+const systemConfig = require("../../config/system.js")
 // const createTreeHelper = require("../../helpers/createTree")
 
 //1. [GET] admin/roles
 module.exports.index = async (req, res)=>{
-
     const find = {
         deleted: false
     }
@@ -17,4 +16,24 @@ module.exports.index = async (req, res)=>{
         pageTitle: "Nhóm quyền",
         records: records
     })
+}
+
+//2. 
+// [GET] admin/roles/create
+module.exports.create = async (req, res)=>{
+    const find = {
+        deleted: false
+    }
+    const records = await Roles.find(find)
+    res.render("admin/pages/roles/create.pug", {
+        pageTitle: "Tạo nhóm quyền",
+        records: records
+    })
+}
+
+//[POST] admin/roles/create
+module.exports.createPost = async (req, res)=>{
+    const userRole =  new Roles(req.body)
+    await userRole.save()
+    res.redirect(`${systemConfig.prefixAdmin}/roles`)
 }
--- routes/admin/role.route.js
+++ routes/admin/role.route.js
@@ -3,7 +3,12 @@ const router = express.Router()
 
 const controller = require("../../controllers/admin/role.controller.js")
 
+//Trang chính
 router.get('/', controller.index) 
+
+//Tạo nhóm quyền
+router.get('/create', controller.create) 
+router.post('/create', controller.createPost) 
 
 module.exports = router
 
