884e881540df077ba963ea084ebc720f29f5a790
Trương Tiến Anh <truongtienanh16@gmail.com>
2024-09-30 22:04:13 +0700

    Update

---
+++ public/admin/js/roles.js
@@ -0,0 +1,10 @@
+//Permission
+
+const tablePermission = document.querySelector("[table-permission]")
+if(tablePermission){
+    const buttonSubmit = document.querySelector("[button-submit]") 
+    
+    buttonSubmit.addEventListener("click", ()=>{
+        
+    })
+}
