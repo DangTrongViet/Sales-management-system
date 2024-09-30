1e72f74ccf901cc19bec151d7c3eae2e973578b9
Trương Tiến Anh <truongtienanh16@gmail.com>
2024-09-30 23:09:51 +0700

    Update

--- public/admin/js/roles.js
+++ public/admin/js/roles.js
@@ -5,6 +5,30 @@ if(tablePermission){
     const buttonSubmit = document.querySelector("[button-submit]") 
     
     buttonSubmit.addEventListener("click", ()=>{
-        
+        let permission = []
+        const rows = tablePermission.querySelectorAll("[data-name]")
+        rows.forEach(row =>{
+            const name = row.getAttribute("data-name")
+            const inputs = row.querySelectorAll("input")
+
+            if(name == "id"){
+                inputs.forEach(input =>{
+                    const id = input.value;
+                    permission.push({
+                        id: id,
+                        permission: []
+                    })
+                })
+            }else{
+                inputs.forEach((input, index) =>{
+                    const checked = input.checked
+                    if(checked==true){
+                        permission[index].permission.push(name)
+                    }
+                    // console.log(name)
+                })
+            }
+            console.log(permission)
+        })
     })
 }
