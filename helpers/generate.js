14b01826f039fa42fb4afcb4c760bd9c82fb78f8
Trương Tiến Anh <truongtienanh16@gmail.com>
2024-10-07 23:30:47 +0700

    Update

--- helpers/generate.js
+++ helpers/generate.js
@@ -5,6 +5,5 @@ module.exports.generateRandomString = (length) =>{
     for(let i = 0; i < length; i++){
         result += characters.charAt(Math.floor(Math.random() * characters.length))
     }
-
     return result;
 }
--- models/account.model.js
+++ models/account.model.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
-
+const generate = require("../helpers/generate.js")
 const accountSchema = new mongoose.Schema(
   {
     fullName: String,
     email: String,
     password: String,
-    token: String,
+    token: {
+        type: String,
+        default: generate.generateRandomString(20)
+    },
     phone: String,
     avatar: String,
     role_id: String,
