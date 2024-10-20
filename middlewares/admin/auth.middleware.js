55f526aeccfe6c5e87224eebfc569e891f13b485
Trương Tiến Anh <truongtienanh16@gmail.com>
2024-10-20 22:15:12 +0700

    Update

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -1,5 +1,7 @@
 const Account = require("../../models/account.model.js")
+const Role = require("../../models/role.model.js")
 const systemConfig = require("../../config/system.js")
+const { application } = require("express")
 
 //Tạo middleware để kiếm tra nếu có token thì mưới cho đăng nhập vô xem các trang
 module.exports.requireAuth = async(req, res, next)=>{
@@ -7,10 +9,16 @@ module.exports.requireAuth = async(req, res, next)=>{
     if(!req.cookies.token){
         res.redirect(`${systemConfig.prefixAdmin}/auth/login`)
     }else{
-        const user = await Account.findOne({token: req.cookies.token})
+        const user = await Account.findOne({token: req.cookies.token}).select("-password")
         if(!user){
             res.redirect(`${systemConfig.prefixAdmin}/auth/login`)
         }else{
+            const role = await Role.findOne({
+                _id: user.role_id
+            })
+            //Trả thông tin user và role về cho frontend
+            res.locals.user = user;
+            res.locals.role = role;
             next()
         }
     }
