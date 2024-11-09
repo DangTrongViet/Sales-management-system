c3370b67f3f0761d901f047b9603f22b4f5d9f3c
Trương Tiến Anh <truongtienanh16@gmail.com>
2024-11-09 23:04:30 +0700

    update

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,6 +1,16 @@
+const ProductCategory = require("../../models/product-category.model")
+const systemConfig = require("../../config/system")
+const createTreeHelper = require("../../helpers/createTree")
 
-module.exports.index =  (req, res)=>{ //Cái này là cú pháp đặt tên hàm trong Nodejs, cụ thẻ ở đây ta đặt tên hàm là "index"
+module.exports.index =  async(req, res)=>{ //Cái này là cú pháp đặt tên hàm trong Nodejs, cụ thẻ ở đây ta đặt tên hàm là "index"
+    let find = {
+        deleted: false
+    };
+    const productCategory = await ProductCategory.find(find)
+    const newProductCategory = createTreeHelper.tree(productCategory)
+
     res.render("client/pages/home/index.pug", {
-        pageTitle: "Tranh chủ"
+        pageTitle: "Tranh chủ",
+        layoutProductCategory: newProductCategory
     })
 };
