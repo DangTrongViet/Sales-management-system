b452adc9acced2a79f9d757f5a43ac664e6e6caf
Trương Tiến Anh <truongtienanh16@gmail.com>
2024-11-09 23:30:51 +0700

    Update

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,7 +1,10 @@
 const productRoutes = require("./product.route.js")
 const homeRoutes = require("./home.route.js")
+const categoryMiddleware = require("../../middlewares/client/category.middleware.js")
+
 //Tách riêng các thằng routes này ra để dễ quản lý
 module.exports = (app)=>{ //Lệnh này là exports trong express
+    app.use(categoryMiddleware.category)
     app.use('/', homeRoutes)//Thay chữ get -> use, nói chung get cũng vẫn chạy được thôi nhưng k cần thiết
     app.use("/products", productRoutes)
 }
