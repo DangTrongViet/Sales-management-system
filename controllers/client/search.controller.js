b310dfa8c1bd7dc0f39a9265e7c591cfd2fd16a9
Trương Tiến Anh <truongtienanh16@gmail.com>
2025-02-04 21:50:56 +0700

    Search update

--- controllers/client/search.controller.js
+++ controllers/client/search.controller.js
@@ -1,11 +1,27 @@
 const Product = require("../../models/product.model.js")
-const productHelper = require("../../helpers/product")
-const ProductCategory = require("../../models/product-category.model.js")
-const productCategoryHelper = require("../../helpers/products-category.js")
+const searchProductHelper = require("../../helpers/search.js")
+const searchHelper = require("../../helpers/product.js")
 
 //1. Hiển thị kết quả tìm kiếm
 module.exports.index = async (req, res)=>{
+    const keyword = req.query.keyword
+    let newProduct = []
+
+    if (keyword){
+        const keywordRegex = new RegExp(keyword, "i")
+        const products = await Product.find({
+            deleted: false,
+            status: "active",
+            title: keywordRegex
+        })
+
+        newProduct = searchHelper.priceNewProducts(products)
+        //console.log(products)
+    }
+
     res.render("client/pages/search/index.pug", {
         pageTitle: "Kết quả tìm kiếm",
+        keyword: keyword,
+        products: newProduct
     })
 }
