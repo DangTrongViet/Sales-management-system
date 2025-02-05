c6c7573893936311e1ed41b43b05e3752bf5225f
Trương Tiến Anh <truongtienanh16@gmail.com>
2025-02-05 13:04:28 +0700

    Alert

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,15 @@
-console.log("Tui ở đây")
+
+//Show alert (Thông báo cập nhập thành công hay chưa)
+const showAlert = document.querySelector("[show-alert]")
+
+if(showAlert){
+   const dataTime = showAlert.getAttribute("data-time")
+   const closeAlert = showAlert.querySelector("[close-alert]")
+   setTimeout(()=>{
+       showAlert.classList.add("alert-hidden")
+   }, parseInt(dataTime))
+   
+   closeAlert.addEventListener("click", ()=>{
+       showAlert.classList.add("alert-hidden")
+   })
+}
