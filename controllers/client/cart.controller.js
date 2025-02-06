92bf4ac14334e67b74ada135f663cc34c5084382
Trương Tiến Anh <truongtienanh16@gmail.com>
2025-02-06 11:05:21 +0700

    Update

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,6 +1,6 @@
 const Product = require("../../models/product.model.js")
 const Cart = require("../../models/cart.model")
-
+const productHelper = require("../../helpers/product.js")
 
 // [GET] /cart
 module.exports.index = async(req, res)=>{
@@ -9,6 +9,27 @@ module.exports.index = async(req, res)=>{
     const cart = await Cart.findOne({
         _id: cartId
     })
+
+    //tổng tiền mỗi đơn hàng
+
+    if(cart.products.length > 0){
+        for(const item of cart.products){
+            const productId = item.product_id
+
+            const productInfor = await Product.findOne({
+                _id: productId
+            })
+
+            productInfor.priceNew = productHelper.priceNewProduct(productInfor)
+
+            item.productInfor = productInfor
+
+            item.totalPrice = item.quantity * productInfor.priceNew
+        }
+    }
+
+    //Tổng tiền của all đơnhàng
+    cart.totalPrice = cart.products.reduce((sum, item) => sum + item.totalPrice, 0)
 
     res.render("client/pages/cart/index", {
         pageTitle: "Giỏ hàng",
