89a70c7795c03dcd582742602d93adf334c927b3
Trương Tiến Anh <truongtienanh16@gmail.com>
2025-02-07 11:55:49 +0700

    Register

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -1,9 +1,36 @@
-const Product = require("../../models/product.model.js")
+const md5 = require("md5")
 const User = require("../../models/user.model.js")
-const productHelper = require("../../helpers/product.js")
 
-// [GET] /cart
+// [GET] /user/register
 module.exports.register = async(req, res)=>{
-    res.send("oke")
+    res.render("client/pages/user/register", {
+        pageTitle: "Đăng ký tài khoản",
+    })
 }
+
+//[POST] /user/register
+module.exports.registerPost = async(req, res)=>{
+    const existEmail = await User.findOne({
+        email: req.body.email,
+        deleted: false
+    })
+
+    if(existEmail){
+        req.flash('error', 'Email đẫ tồn tại!');
+        res.redirect("back")
+        return;
+    }
+
+    req.body.password = md5(req.body.password)
+
+    const user = new User(req.body)
+    await user.save()
+
+    // console.log(user)
+    res.cookie("tokenUser", user.tokenUser)
+    req.flash('success', 'Đăng ký tài khoản thành công!');
+
+    res.redirect("/") 
+}
+
 
