a685047bffcff60f72f4cb418d08cf05821ebd8b
Trương Tiến Anh <truongtienanh16@gmail.com>
2025-02-07 11:32:28 +0700

    Update

---
+++ controllers/client/user.controller.js
@@ -0,0 +1,9 @@
+const Product = require("../../models/product.model.js")
+const User = require("../../models/user.model.js")
+const productHelper = require("../../helpers/product.js")
+
+// [GET] /cart
+module.exports.register = async(req, res)=>{
+    res.send("oke")
+}
+
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -5,6 +5,7 @@ const searchRoutes = require("./search.route.js")
 const cartMiddleware = require("../../middlewares/client/cart.middleware.js")
 const cartRoute = require("./cart.route.js")
 const checkoutRoute = require("./checkout.route.js")
+const userRoute = require("./user.route.js")
 
 //Tách riêng các thằng routes này ra để dễ quản lý
 module.exports = (app)=>{ //Lệnh này là exports trong express
@@ -21,4 +22,6 @@ module.exports = (app)=>{ //Lệnh này là exports trong express
     app.use("/products", productRoutes)
     
     app.use("/search", searchRoutes)
+
+    app.use("/user", userRoute)
 }
