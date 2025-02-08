77ec6bc63feff63e356f974c8e8c823f4d837d3e
Trương Tiến Anh <truongtienanh16@gmail.com>
2025-02-08 20:45:42 +0700

    setting general

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -7,7 +7,7 @@ const cartRoute = require("./cart.route.js")
 const checkoutRoute = require("./checkout.route.js")
 const userRoute = require("./user.route.js")
 const userMiddleware = require("../../middlewares/client/user.middleware.js")
-
+const settingMiddleware = require("../../middlewares/client/setting.middleware.js")
 //Tách riêng các thằng routes này ra để dễ quản lý
 module.exports = (app)=>{ //Lệnh này là exports trong express
     app.use(categoryMiddleware.category)
@@ -15,6 +15,8 @@ module.exports = (app)=>{ //Lệnh này là exports trong express
     app.use(cartMiddleware.cartId)
 
     app.use(userMiddleware.infoUser)
+
+    app.use(settingMiddleware.settingGeneral)
 
     app.use('/', homeRoutes)
 
