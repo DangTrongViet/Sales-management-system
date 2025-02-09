933ab15d80ff08f599672b5d0b208f6ccf618611
Trương Tiến Anh <truongtienanh16@gmail.com>
2025-02-09 19:44:38 +0700

    chat

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -5,6 +5,9 @@ module.exports.index = async(req, res)=>{
     _io.on('connection', (socket) => {
         console.log('a user connected', socket.id);
     })
+    //End SocketIo
+
+    
 
     res.render("client/pages/chat/index", {
         pageTitle: "Chat",
