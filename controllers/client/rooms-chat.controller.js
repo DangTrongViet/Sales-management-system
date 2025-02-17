973f70958e0041b71fc105854ff7aa28850c3759
Trương Tiến Anh <truongtienanh16@gmail.com>
2025-02-17 21:31:50 +0700

    update

--- controllers/client/rooms-chat.controller.js
+++ controllers/client/rooms-chat.controller.js
@@ -1,7 +1,6 @@
 const Chat = require("../../models/chat.model")
 const User = require("../../models/user.model")
-const uploadToClouddinary = require("../../helpers/uploadToClouddinary")
-const chatSocket = require("../../sockets/client/chat.socket")
+const RoomChat = require("../../models/rooms-chat.model")
 
 //[GET] /rooms-chat
 module.exports.index = async(req, res)=>{
@@ -12,7 +11,47 @@ module.exports.index = async(req, res)=>{
 
 //[GET] rooms-chat/create
 module.exports.create = async(req, res)=>{
+    const friendList = res.locals.user.friendList
+
+    for(const friend of friendList){
+        const infoFriend = await User.findOne({
+            _id: friend.user_id
+        }).select("fullName avatar")
+
+        friend.infoFriend = infoFriend
+    }
+
     res.render("client/pages/rooms-chat/create", {
         pageTitle: "Tạo phòng chat",
+        friendList: friendList
     })
+}
+
+//[POST] rooms-chat/create
+module.exports.createPost = async(req, res)=>{
+    const title = req.body.title
+    const usersId = req.body.usersId
+
+    const dataChat = {
+        title: title,
+        typeRoom: "group", //friend (=2) or group (>2)
+        users: []
+    }
+
+    usersId.forEach(userId => {
+        dataChat.users.push({
+            user_id: userId,
+            role: "user"
+        })
+    })
+
+    dataChat.users.push({
+        user_id: res.locals.user.id,
+        role: "superAdmin"
+    })
+    
+    const room = new RoomChat(dataChat)
+    await room.save()
+
+    res.redirect(`/chat/${room.id}`)
 }
