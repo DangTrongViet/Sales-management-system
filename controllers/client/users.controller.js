4684f4bc2e83af0b229c557c018f6e332ec0f7d9
Trương Tiến Anh <truongtienanh16@gmail.com>
2025-02-18 22:15:01 +0700

    completed project

--- controllers/client/users.controller.js
+++ controllers/client/users.controller.js
@@ -15,16 +15,22 @@ module.exports.notFriend = async(req, res) =>{
 
     const requestFriends = myUser.requestFriends
     const acceptFriends = myUser.acceptFriends
+    const friendList = myUser.friendList
+
+    const friendListId = friendList.map(item => item.user_id)
 
     const users = await User.find({
         $and: [
             {_id: {$ne: userId}},
             {_id: { $nin: requestFriends }},
-            {_id: { $nin: acceptFriends }}
+            {_id: { $nin: acceptFriends }},
+            {_id: { $nin: friendListId }}
         ],
         status: "active",
         deleted: false
     }).select("avatar fullName")
+
+    console.log("users", users)
 
     res.render("client/pages/users/not-friend", {
         pageTitle: "Danh sách người dùng",
@@ -32,7 +38,6 @@ module.exports.notFriend = async(req, res) =>{
     })
 }
 
-//1:00:05
 
 //[GET] users/request
 module.exports.request = async(req, res) =>{
